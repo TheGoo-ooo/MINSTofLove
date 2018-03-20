NeuralNetwork fix, backprop init done

NeuralNetwork/backprop.py
@@ -1,45 +1,70 @@
 from dnn import NeuralNetwork
+import numpy as np
+
+# Work in progress...
 
 class BackPropagation:
-    def __init__(self, *input_data, *solution, neural_network):
-        self.input_data = input_date
-        self.solution = solution
+    """
+    Co = (a(L) - y)^2
+    z(L) = w(L)*a(L-1) + b(L)
+    a(L) = activ(z(L))
+    Influence de b sur Co
+    deriv(Co,b(L)) = deriv(z(L),b(L)) * deriv(a(L),z(L)) * deriv(Co,a(L))
+    """
+    def __init__(self, neural_network, input_data=[], solution=[]):
+        self.input_data = np.array(input_data).astype(float)
+        self.solution = np.array(solution).astype(float)
+        self.output = np.empty_like(self.solution)
         self.dnn = neural_network
-        self.output = []
 
-    # Co = (a(L) - y)^2
-    # z(L) = w(L)*a(L-1) + b(L)
-    # a(L) = activ(z(L))
 
-    # Influence de b sur Co
-    # delta(Co,b(L)) = delta(z(L),b(L)) * delta(a(L),z(L)) * delta(Co,a(L))
+    def __str__(self):
+        """ A simple display for small input/output/solution."""
 
-    def initOutput():
+        accu = "BackPropagation Object: " + str(len(self.input_data[0]))
+        accu +=  " input(s), " + str(len(self.output[0])) + " output(s).\n"
+
+        accu += "Input data:\n"
+        for i in range(len(self.input_data)):
+            accu += (str(i) + ": " + str(self.input_data[i]) + "\n")
+
+        accu += "Output data:\n"
+        for i in range(len(self.output)):
+            accu += (str(i) + ": " + str(self.output[i]) + "\n")
+
+        accu += "Solution:\n"
+        for i in range(len(self.solution)):
+            accu += (str(i) + ": " + str(self.solution[i]) + "\n")
+
+        return accu
+
+
+    def initOutput(self):
         """ Gives every input data to the network and saves the output."""
-        for i in range(len(input_data)):
-            dnn.layers[0] = input_data[i]
-            dnn.feedForward()
-            output[i].append(dnn.getOutput())
+        for i in range(len(self.input_data)):
+            self.dnn.layers[0] = self.input_data[i]
+            self.dnn.feedForward()
+            self.output[i] = self.dnn.getOutput()
 
 
-    def getCost():
+    def getCost(self):
         """ Calculate the 'cost', error squared."""
-        return np.power(output[self.dnn.getOutput()]-solution[i],2)
+        return np.power(output[i]-solution[i],2)
 
 
-    def costDerivate():
+    def costDerivate(self):
         """ Partial derivative of cost over output."""
-        return 2*(output-solution)
+        return 2*(self.solution -self.output)
 
 
-    def outputDerivate():
+    def outputDerivate(self):
         """
         Partial derivative of output over z.
         """
         return derivateOfActivationFunction(z)
 
 
-    def zDerivateOverWeight():
+    def zDerivateOverWeight(self):
         """
         Partial derivative of z over weight.
         'z' being: sum(weight(i)*output(i-1)) + bias(i).
@@ -47,7 +72,7 @@ class BackPropagation:
         return a(L-1)
 
 
-    def zDerivateOverBias():
+    def zDerivateOverBias(self):
         """
         Partial derivative of z over bias (always 1).
         'z' being: sum(weight(i)*output(i-1)) + bias(i).
@@ -55,7 +80,7 @@ class BackPropagation:
         return 1
 
 
-    def zDerivateOverOutput():
+    def zDerivateOverOutput(self):
         """
         Partial derivative of z over last output.
         'z' being: sum(weight(i)*output(i-1)) + bias(i).

NeuralNetwork/dnn.py
@@ -53,22 +53,36 @@ class NeuralNetwork:
         return accu
 
 # Activation funcitons.
-    def sigmoid(self, *values):
+    def sigmoid(self, *values, derivate=False):
+        if derivate is True:
+            return [(np.exp(-x))/ float(np.power(1+np.exp(-x), 2))
+                    for x in values]
         return [1/float(1+np.exp(-x)) for x in values]
 
 
-    def tanh(self, *values):
-        return [2*x-1 for x in self.sigmoid(*values)]
+    def tanh(self, *values, derivate=False):
+        if derivate is True:
+            return [1/float(np.power(np.cosh(x),2)) for x in values]
+        return [np.tanh(x) for x in values]
 
 
-    def softmax(self, *values):
+    def softmax(self, *values, derivate=False):
+        if derivate is True:
+            print("(Softmax) This function has no derivative "+
+                  " asigned to it for now.")
+            return 1
         return np.exp(values) / float(sum(np.exp(values)))
 
 
-    def relu(self, *values):
+
+    def relu(self, *values, derivate=False):
+        if derivate is True:
+            return [ 0 if x<0 else 1 for x in values]
         return [np.maximum(0, x) for x in values]
 
-    def none(self, *values):
+    def none(self, *values, derivate=False):
+        if derivate is True:
+            return [1 for x in values]
         return values
 
 # Public funcitons.
@@ -85,6 +99,7 @@ class NeuralNetwork:
     def initLayer(self, *neurons_per_layers):
         for nb_neurons in neurons_per_layers:
             self.__makeLayer(nb_neurons)
+        self.__current_layer = 0
 
 
     def getOutput(self):
@@ -109,7 +124,6 @@ class NeuralNetwork:
     def __makeLayer(self, nb_neurons):
         # Create layer.
         self.layers.append(np.zeros(nb_neurons))
-
 
         if self.__current_layer is not 0:
             last_layer_len = len(self.layers[self.__current_layer-1])